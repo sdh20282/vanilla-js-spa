a5471460a7957c848fb054a80121c1521ba57092
sdh20282 <sdh20282@gmail.com>
2024-01-11 18:53:12 +0900

    Feat : lazy loading 구현

--- src/router.js
+++ src/router.js
@@ -1,8 +1,3 @@
-import Dashboard from "./pages/dashboard/Dashboard.js";
-import Post from "./pages/post/Post.js";
-import Setting from "./pages/setting/Setting.js";
-import NotFound from "./pages/notfound/NotFound.js";
-
 const navigateTo = (url) => {
   history.pushState(null, null, url);
 
@@ -10,11 +5,12 @@ const navigateTo = (url) => {
 }
 
 const router = async () => {
+  // lazy loading 적용
   const routes = [
-    { path: "/", page: Dashboard },
-    { path: "/posts", page: Post },
-    { path: "/settings", page: Setting },
-    { path: "/404", page: NotFound },
+    { path: "/", page: () => import("./pages/dashboard/Dashboard.js") },
+    { path: "/posts", page: () => import("./pages/post/Post.js") },
+    { path: "/settings", page: () => import("./pages/setting/Setting.js") },
+    { path: "/404", page: () => import("./pages/notfound/NotFound.js") },
   ];
 
   // 현재 url과 일치하는 경로의 페이지 획득
@@ -25,8 +21,14 @@ const router = async () => {
     match = routes[routes.length - 1];
   }
 
-  // 해당 페이지 클래스 생성 후 render
-  document.querySelector('#app').innerHTML = await (new match.page()).render();
+  // 렌더링 에러 처리
+  try {
+    // 해당 페이지 클래스 생성 후 render
+    document.querySelector('#app').innerHTML = await (new (await match.page()).default).render();
+  } catch (error) {
+    // 추후 에러 페이지 작성 후 렌더링할 것
+    console.log(error);
+  }
 };
 
 const initRouter = () => {
@@ -36,7 +38,7 @@ const initRouter = () => {
   });
 
   document.addEventListener("DOMContentLoaded", () => {
-    // 클릭 이벤트가 발생했을 때, 해당 target이 a 태그라면 페이지 이동 함수 실행
+    // 클릭 이벤트가 발생했을 때, 해당 target이 data-link 속성을 가지고 있는 경우라면 페이지 이동 함수 실행
     // 별도로 history 설정해주는 함수를 만들어서 반환해서 사용해도 동작하지만 그렇게 할 경우 웹 접근성을 지키기 어렵고,(모든 a 태그의 동작을 막고, 해당 함수로 대체해야 한다.) 관리하기 버겁다고 생각됨
     document.body.addEventListener("click", e => {
       if (e.target.matches("[data-link]")) {
