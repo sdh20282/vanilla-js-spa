7dbd4168a35f1d0119e793b95d8d8f80ec82c462
sdh20282 <sdh20282@gmail.com>
2024-08-16 10:44:00 +0900

    refactor: 불필요한 주석 제거

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const path = require("path");
 
 const app = express();
 
-// middleware 설정
-// '/'으로 시작되는 경로로 접속 시 src가 기본 고정 경로로 고정
 app.use("/", express.static(path.resolve(__dirname, "src", "")))
 
-// SPA이기 때문에, 모든 경로에서 index.html 호출
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "src", "index.html"));
 });
--- src/route.js
+++ src/route.js
@@ -1,15 +1,3 @@
-// import Dashboard from "./pages/dashboard/Dashboard.js";
-// import Post from "./pages/post/Post.js";
-// import Setting from "./pages/setting/Setting.js";
-// import NotFound from "./pages/notfound/NotFound.js";
-
-// export const routes = {
-//   "/": Dashboard,
-//   "/posts": Post,
-//   "/settings": Setting,
-//   "/404": NotFound,
-// };
-
 export const routes = {
   "/": () => import("./pages/dashboard/Dashboard.js"),
   "/posts": () => import("./pages/post/Post.js"),
